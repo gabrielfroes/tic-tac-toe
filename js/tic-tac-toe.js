9c1854e21c02d96184e3d7f808f436f965d894bb
Lucas Bittencourt <lucasnaja0@gmail.com>
2019-04-11 12:09:43 -0300

    restart button added & more

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,57 +1,54 @@
 // TIC TAC TOE
 const tic_tac_toe = {
-
     // ATTRIBUTES
-    board: ['','','','','','','','',''],
+    board: ['', '', '', '', '', '', '', '', ''],
     simbols: {
-                options: ['O','X'],
-                turn_index: 0,
-                change: function(){
-                    this.turn_index = ( this.turn_index === 0 ? 1:0 );
-                }
-            },
+        options: ['O', 'X'],
+        turn_index: 0,
+        change: function () {
+            this.turn_index = (this.turn_index === 0 ? 1 : 0);
+        }
+    },
     container_element: null,
     gameover: false,
     winning_sequences: [
-                        [0,1,2],
-                        [3,4,5],
-                        [6,7,8],
-                        [0,3,6],
-                        [1,4,7],
-                        [2,5,8],
-                        [0,4,8],
-                        [2,4,6]
-                    ],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ],
 
     // FUNCTIONS
-    init: function(container) {
+    init: function (container) {
         this.container_element = container;
     },
 
-    make_play: function(position) {
+    make_play: function (position) {
         if (this.gameover) return false;
-        if (this.board[position] === ''){
+        if (this.board[position] === '') {
             this.board[position] = this.simbols.options[this.simbols.turn_index];
             this.draw();
-            let winning_sequences_index = this.check_winning_sequences( this.simbols.options[this.simbols.turn_index] );
-            if (winning_sequences_index >= 0){
+            let winning_sequences_index = this.check_winning_sequences(this.simbols.options[this.simbols.turn_index]);
+            if (winning_sequences_index >= 0) {
                 this.game_is_over();
-            } else{
+            } else {
                 this.simbols.change();
             }
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     },
 
-    check_winning_sequences: function(simbol) {
-
-        for ( i in this.winning_sequences ) {
-            if (this.board[ this.winning_sequences[i][0] ] == simbol  &&
-                this.board[ this.winning_sequences[i][1] ] == simbol &&
-                this.board[ this.winning_sequences[i][2] ] == simbol) {
+    check_winning_sequences: function (simbol) {
+        for (let i in this.winning_sequences) {
+            if (this.board[this.winning_sequences[i][0]] == simbol &&
+                this.board[this.winning_sequences[i][1]] == simbol &&
+                this.board[this.winning_sequences[i][2]] == simbol) {
                 console.log('winning sequences INDEX:' + i);
                 return i;
             }
@@ -59,24 +56,44 @@ const tic_tac_toe = {
         return -1;
     },
 
-    game_is_over: function() {
+    game_is_over: function () {
         this.gameover = true;
         console.log('GAME OVER');
     },
 
-    start: function() {
-        this.board.fill('');
-        this.draw();
-        this.gameover = false;       
+    is_game_over: function () {
+        for (let i of this.board) {
+            if (i === '') {
+                return false;
+            }
+        }
+
+        return true;
     },
 
-    draw: function() {
+    start: function () {
+        this.board.fill('');
+        this.draw();
+        this.gameover = false;
+    },
+
+    restart: function () {
+        if (this.is_game_over() || this.gameover) {
+            this.start();
+            console.log('this game has been restarted!')
+        } else if (confirm('Are you sure you want to restart this game?')) {
+            this.start();
+            console.log('this game has been restarted!')
+        }
+    },
+
+    draw: function () {
         let content = '';
 
-        for ( i in this.board ) {
-            content += '<div onclick="tic_tac_toe.make_play(' + i + ')">' + this.board[i] + '</div>';
+        for (i in this.board) {
+            content += `<div onclick="tic_tac_toe.make_play(${i})">${this.board[i]}</div>`;
         };
 
         this.container_element.innerHTML = content;
-    },
+    }
 };
