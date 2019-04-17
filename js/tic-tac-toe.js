82a17119d1f2aebd3d328da06f191f2ef85a551c
WilgnerFSDev <wilgner.fabio@gmail.com>
2019-04-17 01:58:35 -0300

    Adicionado Score, Mensagem do Vencedor e botão de jogar novamente

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -3,6 +3,11 @@ const tic_tac_toe = {
 
     // ATTRIBUTES
     board: ['','','','','','','','',''],
+    player_o_wins: 0,
+    player_x_wins: 0,
+    player_o_element: null,
+    player_x_element: null,
+    won_message_element: null,
     symbols: {
                 options: ['O','X'],
                 turn_index: 0,
@@ -24,8 +29,11 @@ const tic_tac_toe = {
                     ],
 
     // FUNCTIONS
-    init: function(container) {
+    init: function(container, player_o_text, player_x_text, won_message_text) {
         this.container_element = container;
+        this.player_o_element = player_o_text;
+        this.player_x_element = player_x_text;
+        this.won_message_element = won_message_text;
     },
 
     make_play: function(position) {
@@ -49,7 +57,7 @@ const tic_tac_toe = {
     check_winning_sequences: function(simbol) {
 
         for ( i in this.winning_sequences ) {
-            if (this.board[ this.winning_sequences[i][0] ] == simbol  &&
+            if (this.board[ this.winning_sequences[i][0] ] == simbol &&
                 this.board[ this.winning_sequences[i][1] ] == simbol &&
                 this.board[ this.winning_sequences[i][2] ] == simbol) {
                 console.log('winning sequences INDEX:' + i);
@@ -61,10 +69,13 @@ const tic_tac_toe = {
 
     game_is_over: function() {
         this.gameover = true;
-        console.log('GAME OVER');
+        this.symbols.turn_index === 0 ? this.player_o_wins++ : this.player_x_wins++;
+        this.won_message_element.innerHTML = 'The Player '+this.symbols.options[this.symbols.turn_index]+' Won';
+        console.log('GAME END '+this.symbols.options[this.symbols.turn_index]);
     },
 
     start: function() {
+        this.won_message_element.innerHTML = ''; 
         this.board.fill('');
         this.draw();
         this.gameover = false;       
@@ -78,5 +89,7 @@ const tic_tac_toe = {
         };
 
         this.container_element.innerHTML = content;
+        this.player_o_element.innerHTML = "Player O Victorys: "+this.player_o_wins;
+        this.player_x_element.innerHTML = "Player X Victorys: "+this.player_x_wins;
     },
 };
