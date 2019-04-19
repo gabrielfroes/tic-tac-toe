60111ee2d20c7029dad7ce0e28666f1495524075
Joao Antonio Cabral <joaomunizshura@gmail.com>
2019-04-19 17:48:28 -0300

    Editor Config e Botão Start

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -2,30 +2,37 @@
 const tic_tac_toe = {
 
     // ATTRIBUTES
-    board: ['','','','','','','','',''],
+    board: [
+        '','','',
+        '','','',
+        '','',''
+    ],
     symbols: {
-                options: ['O','X'],
-                turn_index: 0,
-                change: function(){
-                    this.turn_index = ( this.turn_index === 0 ? 1:0 );
-                }
-            },
+        options: ['O','X'],
+        turn_index: 0,
+        change: function(){
+            this.turn_index = ( this.turn_index === 0 ? 1:0 );
+        }
+    },
     container_element: null,
+    start_element: null,
     gameover: false,
     winning_sequences: [
-                        [0,1,2],
-                        [3,4,5],
-                        [6,7,8],
-                        [0,3,6],
-                        [1,4,7],
-                        [2,5,8],
-                        [0,4,8],
-                        [2,4,6]
-                    ],
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ],
 
     // FUNCTIONS
-    init: function(container) {
+    init: function(container, start) {
         this.container_element = container;
+        this.start_element = start;
+        console.log("Tic Tac Toe - Init");
     },
 
     make_play: function(position) {
@@ -61,6 +68,7 @@ const tic_tac_toe = {
 
     game_is_over: function() {
         this.gameover = true;
+        console.log(this.container_element);
         console.log('GAME OVER');
     },
 
@@ -74,9 +82,12 @@ const tic_tac_toe = {
         let content = '';
 
         for ( i in this.board ) {
-            content += '<div onclick="tic_tac_toe.make_play(' + i + ')">' + this.board[i] + '</div>';
+            content += '<div id="div' + i + '" onclick="tic_tac_toe.make_play(' + i + ')">' + this.board[i] + '</div>';
         };
 
         this.container_element.innerHTML = content;
+        this.start_element.style.display = "none";
+        this.container_element.style.display = "grid";
+        console.log("Tic Tac Toe - Draw");
     },
 };
