0cb0a1aabc1c0ea17dcf3dd5dbe1c6d417093e62
Carlos Almeida <carlosdaniiel0711@hotmail.com>
2019-04-27 17:34:42 -0300

    Usabilidade

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -29,24 +29,28 @@ const tic_tac_toe = {
     },
 
     make_play(position) {
-        if (this.gameover || this.board[position] !== '') return false;
-
-        const currentSymbol = this.symbols.options[this.symbols.turn_index];
-        this.board[position] = currentSymbol;
-        this.draw();
-
-        const winning_sequences_index = this.check_winning_sequences(currentSymbol);
-        if (this.is_game_over()){
-            this.game_is_over();
-        }
-        if (winning_sequences_index >= 0) {
-            this.game_is_over();
-            this.stylize_winner_sequence(this.winning_sequences[winning_sequences_index]);
+        if (this.gameover){
+        	this.restart()
+        } else if (this.board[position] !== '') {
+        	return false;
         } else {
-            this.symbols.change();
-        }
+	        const currentSymbol = this.symbols.options[this.symbols.turn_index];
+	        this.board[position] = currentSymbol;
+	        this.draw();
 
-        return true;
+	        const winning_sequences_index = this.check_winning_sequences(currentSymbol);
+	        if (this.is_game_over()){
+	            this.game_is_over();
+	        }
+	        if (winning_sequences_index >= 0) {
+	            this.game_is_over();
+	            this.stylize_winner_sequence(this.winning_sequences[winning_sequences_index]);
+	        } else {
+	            this.symbols.change();
+	        }
+
+	        return true;
+        }
     },
 
     stylize_winner_sequence(winner_sequence) {
