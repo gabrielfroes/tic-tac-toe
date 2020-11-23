d9f3efa018c8f5cac388fcfc1d411882d4ab445a
NataliaFrancisca <nathifrancisca@gmail.com>
2020-11-22 22:02:00 -0300

    Tela inicial do jogo, tela de game over com botão de jogar novamente

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,102 +1,174 @@
-// TIC TAC TOE
-const tic_tac_toe = {
+var telaInicial = document.querySelector(".telaInicial");
+var displayGame = document.querySelector(".game");
+var displayOver = document.querySelector(".gameOver");
+var textoVencedor = document.querySelector("#nomeWinner");
+var btnRestart = document.querySelector(".btn");
 
-    // ATTRIBUTES
+var consultaSimbolos = [];
+var valorUm = 0;
+var valorDois = 0;
+
+const jogoVelha = {
     board: ['','','','','','','','',''],
-    symbols: {
-                options: ['O','X'],
-                turn_index: 0,
-                change(){
-                    this.turn_index = ( this.turn_index === 0 ? 1:0 );
+    simbols: { 
+        options: ['X', 'O'],
+        turnIndex : 0,
+        change: function(){
+            this.turnIndex = (this.turnIndex === 0 ? 1 : 0);
+        }
+    },
+    containerElement: null,
+    gameOver: false,
+    winnerSequences: [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ],
+
+    init: function(container){
+        this.containerElement = container;
+        //container vai ser a tela do jogo
+    },
+
+    makePlay: function(position){
+        if(this.gameOver) return false;
+
+        if(this.board[position] === ''){
+            this.board[position] = this.simbols.options [ this.simbols.turnIndex];
+            consultaSimbolos.push(this.simbols.turnIndex);
+            this.draw();
+
+            let winnerSequenceIndex = this.checkWinner( this.simbols.options [this.simbols.turnIndex]);
+
+                if(this.isgameOver()){
+                    this.gameisOver();
+                    textoVencedor.innerHTML = "VELHA!";
                 }
-            },
-    container_element: null,
-    gameover: false,
-    winning_sequences: [
-                        [0,1,2],
-                        [3,4,5],
-                        [6,7,8],
-                        [0,3,6],
-                        [1,4,7],
-                        [2,5,8],
-                        [0,4,8],
-                        [2,4,6]
-                    ],
 
-    // FUNCTIONS
-    init(container) {
-        this.container_element = container;
+                if(winnerSequenceIndex >= 0){
+                    this.gameisOver();
+                    this.efeitoWinner(this.winnerSequences[winnerSequenceIndex]);
+                }
+
+                 else{
+                    this.simbols.change();
+                }
+
+                return true;
+            }
+
     },
 
-    make_play(position) {
-        if (this.gameover || this.board[position] !== '') return false;
-
-        const currentSymbol = this.symbols.options[this.symbols.turn_index];
-        this.board[position] = currentSymbol;
-        this.draw();
-
-        const winning_sequences_index = this.check_winning_sequences(currentSymbol);
-        if (this.is_game_over()){
-            this.game_is_over();
-        }
-        if (winning_sequences_index >= 0) {
-            this.game_is_over();
-            this.stylize_winner_sequence(this.winning_sequences[winning_sequences_index]);
-        } else {
-            this.symbols.change();
-        }
-
-        return true;
+    isgameOver() {
+        return !this.board.includes(''); 
+        // vai adicionar os simbolos de acordo com a posição do quadro ao array board
     },
 
-    stylize_winner_sequence(winner_sequence) {
-        winner_sequence.forEach((position) => {
-          this
-            .container_element
+    efeitoWinner(winnerSequences){
+        winnerSequences.forEach((position) => {
+            this
+            .containerElement
             .querySelector(`div:nth-child(${position + 1})`)
             .classList.add('winner');
+
         });
-      },
 
-    check_winning_sequences(symbol) {
+    },
 
-        for ( i in this.winning_sequences ) {
-            if (this.board[ this.winning_sequences[i][0] ] == symbol  &&
-                this.board[ this.winning_sequences[i][1] ] == symbol &&
-                this.board[ this.winning_sequences[i][2] ] == symbol) {
-                console.log('winning sequences INDEX:' + i);
-                return i;
+    gameisOver(){
+        setTimeout(function(){
+        displayOver.style.display = 'grid';
+        displayGame.style.display = 'none';
+        btnRestart.style.display = 'none';
+
+        this.gameOver = true;
+        console.log("Game over!");
+        },2000);
+
+        for (y in consultaSimbolos){
+
+            if(consultaSimbolos[y] == 0){
+                valorUm++;
+            }
+
+            if(consultaSimbolos[y] == 1){
+                valorDois++;
+            }
+        }
+
+        if(consultaSimbolos[y] == 0 || valorUm - 1 > valorDois){
+            textoVencedor.innerHTML = "O vencedor foi o " +  '<br>' + " X " ;
+        }
+
+        if(consultaSimbolos[y] == 1 || valorDois - 1 > valorUm){
+            textoVencedor.innerHTML = "O vencedor foi o " +  '<br>' + " O " ;
+        } 
+
+        btnRestart.style.display = 'none';
+    },
+
+    jogarNovamente(){
+        displayOver.style.display = 'none';
+        displayGame.style.display = 'grid';
+        btnRestart.style.display = 'flex';
+
+        this.board.fill('');
+        this.draw();
+        this.gameOver = false;
+        valorUm = [];
+        valorDois = [];
+        consultaSimbolos = [];
+    },
+
+    start(){
+        this.board.fill('');
+        this.draw();
+        this.gameOver = false;
+
+        telaInicial.style.display = 'none';
+        displayGame.style.display = 'grid';
+        btnRestart.style.display = 'grid';
+
+    },
+
+    restart(){
+        this.board.fill('');
+        this.draw();
+        this.gameOver = false;
+
+        valorUm = [];
+        valorDois = [];
+        consultaSimbolos = [];  
+    },
+
+    checkWinner(simbol){
+        for ( i in this.winnerSequences){
+            if(this.board [this.winnerSequences[i][0]] == simbol && 
+                this.board [this.winnerSequences[i][1]] == simbol && 
+                this.board [this.winnerSequences[i][2]] == simbol){
+                    return i;
             }
         };
+        
         return -1;
     },
 
-    game_is_over() {
-        this.gameover = true;
-        console.log('GAME OVER');
-    },
-
-    is_game_over() {
-        return !this.board.includes('');
-    },
-
-    start() {
-        this.board.fill('');
-        this.draw();
-        this.gameover = false;       
-    },
-
-    restart() {
-        if (this.is_game_over() || this.gameover) {
-            this.start();
-            console.log('this game has been restarted!')
-        } else if (confirm('Are you sure you want to restart this game?')) {
-            this.start();
-            console.log('this game has been restarted!')
+    draw(){
+        let content = '';
+        for ( i in this.board){
+            content += '<div onclick="jogoVelha.makePlay(' + i + ')">' + this.board[i] + '</div>';
+            // i vai ser os números, do board
+            // content vai ser as divs que serão criadas
         }
-    },
+        this.containerElement.innerHTML = content;
 
-    draw() {
-        this.container_element.innerHTML = this.board.map((element, index) => `<div onclick="tic_tac_toe.make_play('${index}')"> ${element} </div>`).reduce((content, current) => content + current);
-    },
+    }
 };
+
+
+
